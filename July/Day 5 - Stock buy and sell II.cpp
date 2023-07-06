5c558ef38854232d15438e8709258e49f5f1094a
Ajay Rawat <60476050+theajayrawat@users.noreply.github.com>
2023-07-06 08:00:09 +0530

    Update Day 5 - Stock buy and sell II.cpp

--- July/Day 5 - Stock buy and sell II.cpp
+++ July/Day 5 - Stock buy and sell II.cpp
@@ -1,3 +1,19 @@
 // Link:https://practice.geeksforgeeks.org/problems/stock-buy-and-sell2615/1
 // Time Complexity: O(N)
 // Space Complexity: O(1)
+
+class Solution {
+  public:
+    int stockBuyAndSell(int n, vector<int> &prices) {
+        // code here
+        int profit=0;
+
+        for(int i=1;i<n;i++)
+        {
+            if(prices[i-1]<prices[i])
+                profit+=prices[i]-prices[i-1];
+        }
+
+        return profit;
+    }
+};
