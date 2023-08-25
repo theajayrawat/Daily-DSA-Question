271107339a13781557174d1910e6b398c5e2c4ad
Ajay Rawat <60476050+theajayrawat@users.noreply.github.com>
2023-08-25 18:17:59 +0530

    Add files via upload

---
+++ August/Day 25 - Palindrome String.cpp
@@ -0,0 +1,22 @@
+// Link:https://practice.geeksforgeeks.org/problems/palindrome-string0817/1
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+class Solution
+{
+public:
+    int isPalindrome(string S)
+    {
+        // Your code goes here
+        int i = 0;
+        int j = S.length() - 1;
+        while (i < j)
+        {
+            if (S[i] != S[j])
+                return 0;
+            i++;
+            j--;
+        }
+        return 1;
+    }
+};
