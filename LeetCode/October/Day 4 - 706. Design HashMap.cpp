05c121f4b429c05a9d447773d1c4983de685588c
Ajay Rawat <theajayrawat@gmail.com>
2023-10-05 05:34:49 +0530

    day 4

---
+++ LeetCode/October/Day 4 - 706. Design HashMap.cpp
@@ -0,0 +1,23 @@
+// eetcode.com/problems/design-hashmap/
+// TC: O(1)
+// SC: O(N)
+
+class MyHashMap {
+public:
+    int v[1000001];
+    MyHashMap() {
+        fill(v, v + 1000000, -1);
+    }
+    
+    void put(int key, int value) {
+        v[key]=value;
+    }
+    
+    int get(int key) {
+        return v[key];
+    }
+    
+    void remove(int key) {
+        v[key]=-1;
+    }
+};
