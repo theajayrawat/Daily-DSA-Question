c8e8252816bc7936b1687ac6382e02ba4827bbea
Ajay Rawat <theajayrawat@gmail.com>
2023-10-22 23:16:28 +0530

    day22

---
+++ LeetCode/October/Day 22 - 1793. Maximum Score of a Good Subarray.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/maximum-score-of-a-good-subarray/
+// T.C : O(N)
+// S.C : O(1)
+
+class Solution {
+public:
+    int maximumScore(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        int i = k;
+        int j = k;
+        int curMin = nums[k];
+        
+        int result = nums[k];
+        
+        while(i > 0 || j < n-1) {
+            int leftValue;
+            int rightValue;
+            
+            if(i > 0) {
+                leftValue = nums[i-1];
+            } else {
+                leftValue = 0;
+            }
+            
+            if(j < n-1) {
+                rightValue = nums[j+1];
+            } else {
+                rightValue = 0;
+            }
+            
+            if(leftValue < rightValue) { 
+                j++;
+                curMin = min(curMin, nums[j]);
+            } else {
+                i--;
+                curMin = min(curMin, nums[i]);
+            }
+            
+            result = max(result, curMin * (j-i+1));
+        }
+        
+        return result;
+    }
+};
