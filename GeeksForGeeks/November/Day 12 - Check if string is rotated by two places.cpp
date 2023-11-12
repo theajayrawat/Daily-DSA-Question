dbb5d5edcc66669a622e496ab4561270db641454
Ajay Rawat <theajayrawat@gmail.com>
2023-11-12 20:37:04 +0530

    day12

--- GeeksForGeeks/November/Day 12 - Check if string is rotated by two places.cpp
+++ GeeksForGeeks/November/Day 12 - Check if string is rotated by two places.cpp
@@ -2,3 +2,25 @@
 // TC: O(N)
 // SC: O(N)
 
+class Solution
+{
+    public:
+    //Function to check if a string can be obtained by rotating
+    //another string by exactly 2 places.
+    bool isRotated(string str1, string str2)
+    {
+        // Your code here
+        int n1 = str1.size();
+
+        int n2 = str2.size();
+
+        if(n1 != n2)
+            return false;
+
+        string rotatedLeft = str1.substr(2) + str1.substr(0, 2);
+        string rotatedRight = str1.substr(n1 - 2) + str1.substr(0, n1 - 2);
+        
+        return rotatedLeft==str2 || rotatedRight==str2;
+    }
+
+};
