c2e8107a911cce73f6918e909e2a3aca236b72f9
Ajay Rawat <theajayrawat@gmail.com>
2023-11-17 00:11:32 +0530

    images

---
+++ LeetCode/November/Day 16 - Find the String.cpp
@@ -0,0 +1,20 @@
+// https://www.geeksforgeeks.org/problems/find-the-string/1
+// TC: O(N) 
+// SC: O(1) 
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+        
+        string result;
+        
+        for(int i = 0; i<n; i++) {
+            char ch = nums[i][i];
+            
+            result += (ch == '0') ? "1" : "0";
+        }
+        
+        return result;
+    }
+};
