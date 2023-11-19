459a4a8816a7f286c6afcdd83a96eba1c8ffd1b4
Ajay Rawat <theajayrawat@gmail.com>
2023-11-19 19:29:30 +0530

    day19

---
+++ LeetCode/November/Day 19 - 1887. Reduction Operations to Make the Array Elements Equal.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/
+// TC: O(N*LogN)
+// SC: O(1)
+
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+        int n = nums.size();
+        
+        sort(begin(nums), end(nums));
+        
+        int count = 0;
+        
+        for(int i = n-1; i > 0; i--) {
+            if(nums[i] == nums[i-1]) {
+                continue;
+            }
+            
+            count += n-i;
+        }
+        
+        
+        return count;
+    }
+};
+
