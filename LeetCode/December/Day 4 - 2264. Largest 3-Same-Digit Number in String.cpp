3e979bb259382ca17cd989e6c330f27881686e53
Ajay Rawat <theajayrawat@gmail.com>
2023-12-04 21:56:33 +0530

    day4

---
+++ LeetCode/December/Day 4 - 2264. Largest 3-Same-Digit Number in String.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/largest-3-same-digit-number-in-string/
+// TC: O(N)
+// SC: O(1)
+
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        int ans=-1;
+        for(int i=0;i<num.size()-2;i++){
+            if(num[i]==num[i+1] && num[i]==num[i+2]){
+                ans=max(ans,num[i]-'0');
+                i+=2;
+                cout<<num[i]-'0';
+            }
+        }
+
+        return ans==-1?"":to_string(ans)+to_string(ans)+to_string(ans);
+    }
+};
