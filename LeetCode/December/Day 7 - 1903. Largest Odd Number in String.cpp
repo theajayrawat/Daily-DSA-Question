a02f0e3f04e1262d64423b50824cbd0dbc95f3f8
Ajay Rawat <theajayrawat@gmail.com>
2023-12-07 07:02:14 +0530

    day7

---
+++ LeetCode/December/Day 7 - 1903. Largest Odd Number in String.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/largest-odd-number-in-string/
+// TC: O(N)
+// SC: O(1)
+
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        for (int i = num.size() - 1; i >= 0; i--) {
+            if ((num[i] - '0') % 2) {
+                return num.substr(0, i + 1);
+            }
+        }
+        
+        return ""; 
+    }
+};
